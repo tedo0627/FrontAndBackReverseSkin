73028d6f250cd80a1ce9aec3a61aedb141222e77
tedo0627 <tedosw1mpr1ver@gmail.com>
2020-03-13 22:26:45 +0900

    アップロード

---
+++ FrontAndBackReverseSkin/FrontAndBackReverseSkin/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FrontAndBackReverseSkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.basePath = new System.Windows.Forms.TextBox();
+            this.createPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(19, 19);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "実行";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "元のスキンを選択してください";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "保存先を選択してください";
+            // 
+            // basePath
+            // 
+            this.basePath.Location = new System.Drawing.Point(61, 74);
+            this.basePath.Name = "basePath";
+            this.basePath.Size = new System.Drawing.Size(279, 19);
+            this.basePath.TabIndex = 5;
+            // 
+            // createPath
+            // 
+            this.createPath.Location = new System.Drawing.Point(61, 177);
+            this.createPath.Name = "createPath";
+            this.createPath.Size = new System.Drawing.Size(279, 19);
+            this.createPath.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 332);
+            this.Controls.Add(this.createPath);
+            this.Controls.Add(this.basePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "FrontAndBackReverseSkin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox basePath;
+        private System.Windows.Forms.TextBox createPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FrontAndBackReverseSkin/FrontAndBackReverseSkin/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FrontAndBackReverseSkin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "png files (*.png)|*.png";
+            openFileDialog1.Title = "スキンの画像を選択してください";
+            openFileDialog1.FileName = "skin.ping";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                String text = openFileDialog1.FileName;
+                basePath.Text = text;
+                if (text.Contains(".png") && createPath.Text == "")
+                {
+                    createPath.Text = text.Replace(".png", "_reverse.png");
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "png files (*.png)|*.png";
+            openFileDialog1.Title = "スキンの保存先の画像を選択してください";
+            openFileDialog1.FileName = "skin.ping";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                createPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (basePath.Text == "")
+            {
+                MessageBox.Show("元のスキンを選択してください");
+                return;
+            }
+
+            if (!File.Exists(basePath.Text))
+            {
+                MessageBox.Show("元のスキンが見つかりませんでした");
+                return;
+            }
+
+            if (createPath.Text == "")
+            {
+                MessageBox.Show("保存先を選択してください");
+                return;
+            }
+
+            Bitmap read = new Bitmap(basePath.Text);
+            Bitmap write = new Bitmap(basePath.Text);
+
+            Head(read, write, false);
+            Head(read, write, true);
+
+            RightLeg(read, write, false);
+            RightLeg(read, write, true);
+
+            LeftLeg(read, write, false);
+            LeftLeg(read, write, true);
+
+            Body(read, write, false);
+            Body(read, write, true);
+
+            RightArm(read, write, false);
+            RightArm(read, write, true);
+
+            LeftArm(read, write, false);
+            LeftArm(read, write, true);
+
+            write.Save(createPath.Text);
+
+            read.Dispose();
+            write.Dispose();
+
+            MessageBox.Show("処理が完了しました");
+        }
+
+        private void Head(Bitmap read, Bitmap write, Boolean outSide)
+        {
+            CutOut(read, write, 8, 8, 8, 0, 8, 0, RotateFlipType.Rotate180FlipNone, outSide, 32, 0); //上
+            CutOut(read, write, 8, 8, 16, 0, 16, 0, RotateFlipType.Rotate180FlipNone, outSide, 32, 0); //下
+
+            CutOut(read, write, 8, 8, 0, 8, 16, 8, null, outSide, 32, 0); //左
+            CutOut(read, write, 8, 8, 8, 8, 24, 8, null, outSide, 32, 0); //正面
+            CutOut(read, write, 8, 8, 16, 8, 0, 8, null, outSide, 32, 0); //右
+            CutOut(read, write, 8, 8, 24, 8, 8, 8, null, outSide, 32, 0); //後ろ
+        }
+
+        private void RightLeg(Bitmap read, Bitmap write, Boolean outSide)
+        {
+            CutOut(read, write, 4, 4, 4, 16, 4, 16, RotateFlipType.Rotate180FlipNone, outSide, 0, 16); //上
+            CutOut(read, write, 4, 4, 8, 16, 8, 16, RotateFlipType.Rotate180FlipNone, outSide, 0, 16); //下
+
+            CutOut(read, write, 4, 12, 0, 20, 8, 20, null, outSide, 0, 16); //左
+            CutOut(read, write, 4, 12, 4, 20, 12, 20, null, outSide, 0, 16); //正面
+            CutOut(read, write, 4, 12, 8, 20, 0, 20, null, outSide, 0, 16); //右
+            CutOut(read, write, 4, 12, 12, 20, 4, 20, null, outSide, 0, 16); //後ろ
+        }
+
+        private void LeftLeg(Bitmap read, Bitmap write, Boolean outSide)
+        {
+            CutOut(read, write, 4, 4, 4, 48, 4, 48, RotateFlipType.Rotate180FlipNone, outSide, 16, 0); //上
+            CutOut(read, write, 4, 4, 8, 48, 8, 48, RotateFlipType.Rotate180FlipNone, outSide, 16, 0); //下
+
+            CutOut(read, write, 4, 12, 0, 52, 8, 52, null, outSide, 16, 0); //左
+            CutOut(read, write, 4, 12, 4, 52, 12, 52, null, outSide, 16, 0); //正面
+            CutOut(read, write, 4, 12, 8, 52, 0, 52, null, outSide, 16, 0); //右
+            CutOut(read, write, 4, 12, 12, 52, 4, 52, null, outSide, 16, 0); //後ろ
+        }
+
+        private void Body(Bitmap read, Bitmap write, Boolean outSide)
+        {
+            CutOut(read, write, 8, 4, 20, 16, 20, 16, RotateFlipType.Rotate180FlipNone, outSide, 0, 16); //上
+            CutOut(read, write, 8, 4, 28, 16, 28, 16, RotateFlipType.Rotate180FlipNone, outSide, 0, 16); //下
+
+            CutOut(read, write, 4, 12, 16, 20, 28, 20, null, outSide, 0, 16); //左
+            CutOut(read, write, 8, 12, 20, 20, 32, 20, null, outSide, 0, 16); //正面
+            CutOut(read, write, 4, 12, 28, 20, 16, 20, null, outSide, 0, 16); //右
+            CutOut(read, write, 8, 12, 32, 20, 20, 20, null, outSide, 0, 16); //後ろ
+        }
+
+        private void RightArm(Bitmap read, Bitmap write, Boolean outSide)
+        {
+            CutOut(read, write, 4, 4, 44, 16, 44, 16, RotateFlipType.Rotate180FlipNone, outSide, 0, 16); //上
+            CutOut(read, write, 4, 4, 48, 16, 48, 16, RotateFlipType.Rotate180FlipNone, outSide, 0, 16); //下
+
+            CutOut(read, write, 4, 12, 40, 20, 48, 20, null, outSide, 0, 16); //左
+            CutOut(read, write, 4, 12, 44, 20, 52, 20, null, outSide, 0, 16); //正面
+            CutOut(read, write, 4, 12, 48, 20, 40, 20, null, outSide, 0, 16); //右
+            CutOut(read, write, 4, 12, 52, 20, 44, 20, null, outSide, 0, 16); //後ろ
+        }
+
+        private void LeftArm(Bitmap read, Bitmap write, Boolean outSide)
+        {
+            CutOut(read, write, 4, 4, 36, 48, 36, 48, RotateFlipType.Rotate180FlipNone, outSide, 16, 0); //上
+            CutOut(read, write, 4, 4, 40, 48, 40, 48, RotateFlipType.Rotate180FlipNone, outSide, 16, 0); //下
+
+            CutOut(read, write, 4, 12, 32, 52, 40, 52, null, outSide, 16, 0); //左
+            CutOut(read, write, 4, 12, 36, 52, 44, 52, null, outSide, 16, 0); //正面
+            CutOut(read, write, 4, 12, 40, 52, 32, 52, null, outSide, 16, 0); //右
+            CutOut(read, write, 4, 12, 44, 52, 36, 52, null, outSide, 16, 0); //後ろ
+        }
+
+        private void CutOut(Bitmap read, Bitmap write, int pixelX, int pixelY, int cutX, int cutY, int putX, int putY, RotateFlipType? rotateType, Boolean outSide = false, int outOffsetX = 0, int outOffsetY = 0)
+        {
+            Bitmap cut = new Bitmap(pixelX, pixelY);
+            for (int y = 0; y < pixelY; ++y)
+            {
+                for (int x = 0; x < pixelX; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + cutX + (outSide ? outOffsetX : 0), y + cutY + (outSide ? outOffsetY : 0)));
+                }
+            }
+
+            if (rotateType.HasValue)
+            {
+                cut.RotateFlip(rotateType.Value);
+            }
+
+            for (int y = 0; y < pixelY; ++y)
+            {
+                for (int x = 0; x < pixelX; ++x)
+                {
+                    write.SetPixel(x + putX + (outSide ? outOffsetX : 0), y + putY + (outSide ? outOffsetY : 0), cut.GetPixel(x, y));
+                }
+            }
+
+            cut.Dispose();
+        }
+    }
+}
